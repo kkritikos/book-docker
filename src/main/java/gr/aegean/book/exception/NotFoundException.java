a25dafbe52720a7838ba1f16a027aaa19bce7d88
Kyriakos Kritikos <domcretan@gmail.com>
2021-06-10 23:35:07 +0300

    Commit with complete content

---
+++ src/main/java/gr/aegean/book/exception/NotFoundException.java
@@ -0,0 +1,12 @@
+package gr.aegean.book.exception;
+
+import javax.xml.ws.WebFault;
+
+/**
+ * Thrown when a resource requested is not found.
+ *
+ * @author Kyriakos Kritikos
+ */
+@WebFault
+public class NotFoundException extends Exception {
+}
