42df090d70800df06db0fb55cba44d2b91f4f936
Kyriakos Kritikos <domcretan@gmail.com>
2021-07-27 02:14:07 +0300

    Small update to verification file

--- verify.sh
+++ verify.sh
@@ -1,5 +1,5 @@
 sudo docker build -t tomcat-book:latest .
-sudo docker build -t mysql:latest -f Dockerfile_mysql
+sudo docker build -t mysql:latest -f Dockerfile_mysql .
 sudo docker network create book-net
 sudo docker run -d --name mysql --network book-net --network-alias mysql.mysqlns mysql:latest
 sudo docker run -d --name tomcat --network book-net --network-alias tomcat tomcat-book:latest
